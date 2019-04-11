1e05602b3f409f8b3fc721e3c5b202c40549f899
Jesse Boe <JesseBoe@gmail.com>
2019-04-10 21:20:45 -0700

    fix

--- javascript/logic.js
+++ javascript/logic.js
@@ -2,16 +2,6 @@ var Wins = 0;
 var Losses = 0;
 var Ties = 0;
 
-
-
-var url2 = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/cuisine&X-RapidAPI-Host=spoonacular-recipe-food-nutrition-v1.p.rapidapi.com&X-RapidAPI-Key=b2a438b504msh5c44b66f387d373p1fbdadjsn9a8aa9582250&Content-Type=application/x-www-form-urlencoded&ingredientList=lasagna&title=lasagna";
-$.ajax({
-    url : url2,
-    Method : "GET"
-}).then(function(response){
-    console.log(response);
-})
-
 var config = {
     apiKey: "AIzaSyAmqOqNGPGxknuE7b5sj_iy6a1OCGvKMrM",
     authDomain: "trytwo-6f07c.firebaseapp.com",
